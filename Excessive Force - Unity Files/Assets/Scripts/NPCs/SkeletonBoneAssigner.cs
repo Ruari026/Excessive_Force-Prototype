9a0b892dd9dbdf3d981bf4d4ffaa6d2803f80cd3
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2020-09-25 04:16:01 +0100

    hub updates

--- Excessive Force - Unity Files/Assets/Scripts/NPCs/SkeletonBoneAssigner.cs
+++ Excessive Force - Unity Files/Assets/Scripts/NPCs/SkeletonBoneAssigner.cs
@@ -6,16 +6,4 @@ using UnityEngine;
 public class SkeletonBoneAssigner : MonoBehaviour
 {
     public BoneType[] boneTypes;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
